a25cde191df088cecb62638cff9544fc1af37e1a
jawad-elhajjami <jawadelhajjamiofficiel@gmail.com>
2024-07-16 02:06:53 +0100

    Frontend

--- src/main/java/dev/jawad/movies/ReviewController.java
+++ src/main/java/dev/jawad/movies/ReviewController.java
@@ -8,6 +8,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 import java.util.Map;
 
+@CrossOrigin
 @RestController
 @RequestMapping("/api/v1/reviews")
 public class ReviewController {
